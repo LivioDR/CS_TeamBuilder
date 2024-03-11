4ffe88930f46eeb878c617cf5750b9202d01f45e
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-03-11 13:39:02 -0400

    weapon selection working. Displaying skins on button click

--- js/components/weaponSelectionButtons.js
+++ js/components/weaponSelectionButtons.js
@@ -3,8 +3,19 @@ const toggleCategory = () => {
     const weaponsObject = JSON.parse(localStorage.getItem('weaponObject'))
     const weaponsTypeButtons = getWeaponTypeButtonsByCategory(weaponsObject, clickedCategory)
     document.getElementById('weaponTypeDiv').innerHTML = weaponsTypeButtons
-}
 
+    const allWeaponButtons = document.getElementsByTagName('button')
+    for(let i=0; i<allWeaponButtons.length; i++){
+        if(allWeaponButtons[i].id.endsWith('-weaponButton')){
+            allWeaponButtons[i].addEventListener('click',() => {
+                let clickedWeapon = `${allWeaponButtons[i].id.split('-weaponButton')[0]}`
+                localStorage.setItem('selectedWeapon',clickedWeapon)
+                const skinsTypeButtons = getSkinTypeButtonsByWeapon(weaponsObject, clickedWeapon)
+                document.getElementById('skinTypeDiv').innerHTML = skinsTypeButtons
+                })
+            }
+        }
+    }
 
 const getWeponCategoryButtons = (obj) => {
     let keys = Object.keys(obj).filter(key => key != 'null')
@@ -20,10 +31,20 @@ const getWeaponTypeButtonsByCategory = (obj, category) => {
     let weaponTypes = Object.keys(obj[category]).filter(key => key != 'availableRarities')
     let buttonsHtmlString = `<div id='${category}-weapons-div' style="display: flex; flex-direction: column; width: 100%;">`
     for(let i=0; i<weaponTypes.length; i++){
-        buttonsHtmlString += `<button id='${weaponTypes[i]}-weaponButton'>${weaponTypes[i]}</button>`
+        buttonsHtmlString += `<button id='${weaponTypes[i]}-weaponButton'">${weaponTypes[i]}</button>`
     }
     buttonsHtmlString += `</div>`
     return buttonsHtmlString
 }
 
-export {getWeponCategoryButtons, getWeaponTypeButtonsByCategory, toggleCategory}
+const getSkinTypeButtonsByWeapon = (obj, weaponName) => {
+    const category = localStorage.getItem("selectedCategory")
+    const skinsArray = obj[category][weaponName]
+    let skinCards = skinsArray.map(element => {
+        return `<div id='${element.id}'><img src='${element.image}'><button id='${element.id}-button' onclick="localStorage.setItem('selectedSkin','${element.id}')"><p>${element.skinName}</p><p>$ ${element.price}</p></button></div>`
+    });
+    skinCards = skinCards.join('')
+    return skinCards
+}
+
+export {getWeponCategoryButtons, getWeaponTypeButtonsByCategory, getSkinTypeButtonsByWeapon, toggleCategory}
