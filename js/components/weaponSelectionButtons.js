71bf7a831cdfda4decc076ecb5b0e481c0d3cc50
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-03-11 16:28:17 -0400

    skin cards css formatting done

--- js/components/weaponSelectionButtons.js
+++ js/components/weaponSelectionButtons.js
@@ -41,7 +41,7 @@ const getSkinTypeButtonsByWeapon = (obj, weaponName) => {
     const category = localStorage.getItem("selectedCategory")
     const skinsArray = obj[category][weaponName]
     let skinCards = skinsArray.map(element => {
-        return `<div id='${element.id}'><img src='${element.image}'><button id='${element.id}-button' onclick="localStorage.setItem('selectedSkin','${element.id}')"><p>${element.skinName}</p><p>$ ${element.price}</p></button></div>`
+        return `<div id='${element.id}' style="width: 28%; align-text: center; background-color: grey; margin: 2%; aspect-ratio: 1;"><img src='${element.image}' style="width: 100%; margin: 0%; padding: 0%;"><button id='${element.id}-button' onclick="localStorage.setItem('selectedSkin-${category}','${element.id}')" style="margin-bottom: 0%;"><div style="display: flex; flex-direction: row; flex-wrap: wrap;justify-content: space-between;"><p style="color: black; width: 100%; font-size: small; text-align: center; margin: 0%;">${element.skinName}<br>$${element.price}</p></div></button></div>`
     });
     skinCards = skinCards.join('')
     return skinCards
