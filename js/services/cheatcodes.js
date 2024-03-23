fcc9f31a7b9d87cbf1f84bdebf3adde67e21d100
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-03-23 14:13:28 -0400

    removed SSB4 characters while maintenance

--- js/services/cheatcodes.js
+++ js/services/cheatcodes.js
@@ -1,14 +1,15 @@
 const customPortraits = {
     // Smash Bros skins - https://kuroganehammer.com/Smash4
-    'pewpewpew': ['https://kuroganehammer.com/images/smash4/character/character-rockman.png','Megaman'],
-    'chomp chomp': ['https://kuroganehammer.com/images/smash4/character/character-pac-man.png','Pac-Man'],
-    'final fantasy': ['https://kuroganehammer.com/images/smash4/character/character-cloud.png','Cloud Strife'],
-    'hyrule warrior': ['https://kuroganehammer.com/images/smash4/character/character-link.png', 'Link'],
-    'sonic speed': ['https://kuroganehammer.com/images/smash4/character/character-sonic.png', 'Sonic'],
-    'bounty hunter': ['https://kuroganehammer.com/images/smash4/character/character-samus.png', 'Samus Aran'],
-    'italian plumber': ['https://kuroganehammer.com/images/smash4/character/character-mario.png', 'Mario'],
-    'monkey business': ['https://kuroganehammer.com/images/smash4/character/character-donkey_kong.png','Donkey Kong'],
-    'ohip': ['https://kuroganehammer.com/images/smash4/character/character-dr_mario.png','Dr. Mario'],
+    // Page currently under maintenance
+    // 'pewpewpew': ['https://kuroganehammer.com/images/smash4/character/character-rockman.png','Megaman'],
+    // 'chomp chomp': ['https://kuroganehammer.com/images/smash4/character/character-pac-man.png','Pac-Man'],
+    // 'final fantasy': ['https://kuroganehammer.com/images/smash4/character/character-cloud.png','Cloud Strife'],
+    // 'hyrule warrior': ['https://kuroganehammer.com/images/smash4/character/character-link.png', 'Link'],
+    // 'sonic speed': ['https://kuroganehammer.com/images/smash4/character/character-sonic.png', 'Sonic'],
+    // 'bounty hunter': ['https://kuroganehammer.com/images/smash4/character/character-samus.png', 'Samus Aran'],
+    // 'italian plumber': ['https://kuroganehammer.com/images/smash4/character/character-mario.png', 'Mario'],
+    // 'monkey business': ['https://kuroganehammer.com/images/smash4/character/character-donkey_kong.png','Donkey Kong'],
+    // 'ohip': ['https://kuroganehammer.com/images/smash4/character/character-dr_mario.png','Dr. Mario'],
     // Fortnite skins - https://fortnite.fandom.com/wiki/Outfits
     'dark force': ['https://static.wikia.nocookie.net/fortnite/images/9/95/Darth_Vader_-_Outfit_-_Fortnite.png','Darth Vader'],
     'lh44blessed': ['https://static.wikia.nocookie.net/fortnite/images/b/b1/Lewis_Hamilton_-_Outfit_-_Fortnite.png','Lewis Hamilton'],
