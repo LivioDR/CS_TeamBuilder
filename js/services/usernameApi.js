aa413c5b3587858f138216d76c9b52603eaa471b
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-04-08 14:20:45 -0400

    removed not used function and added comments to file

--- js/services/usernameApi.js
+++ js/services/usernameApi.js
@@ -3,21 +3,13 @@ const availableCountries = [
     'AU', 'BR', 'CA', 'CH', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'IE', 'IN', 'IR', 'MX', 'NL', 'NO', 'NZ', 'RS', 'TR', 'UA', 'US'
 ]
 
-// I read first the navigator language to retrieve a users that may be connected to the player's nearest server 
+// I read first the navigator language to retrieve users that simulate being connected to the player's nearest server
 const navigatorLanguage = navigator.language
+// If the browser language is not among the supported languages, I pick US-English by default
 let nat = availableCountries.includes(navigatorLanguage.split('-')[0].toUpperCase()) ? navigatorLanguage.split('-')[0].toUpperCase() : 'US'
 
-
-const getFullUserData = async() => {
-    try{
-        let result = await fetch(`${baseUrl}/api?nat=${nat}`).then(res => res.json())
-        return result
-    }
-    catch(e){
-        return e
-    }
-}
-
+// Returns an array of 7 users with their name and username to be used while creating my teammates and the users for the rival team
+// Since I'll be requiring always only 7 users, I'm setting this on the API call instead of this number being an argument that the function migth receive
 const getUserFullNameAndUsername = async() => {
     try{
         let result = await fetch(`${baseUrl}/api?results=7&nat=${nat}&inc=name,login`).then(res => res.json())
@@ -28,4 +20,4 @@ const getUserFullNameAndUsername = async() => {
     }
 }
 
-export {getFullUserData, getUserFullNameAndUsername}
+export {getUserFullNameAndUsername}
