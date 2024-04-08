1488bd0e6fbd3fca3c6af030e0b3b2e3cca3fa44
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-04-08 14:20:22 -0400

    removed not used functions and added comments to file

--- js/services/csgoApiSkins.js
+++ js/services/csgoApiSkins.js
@@ -4,84 +4,27 @@ import weaponPricing from "../utilities/weaponPricing.js"
 const language = 'en'
 const baseUrl = `https://bymykel.github.io/CSGO-API/api/${language}`
 
-const getAllSkins = async() => {
-    try{
-        let result = await fetch(`${baseUrl}/skins.json`).then(res => res.json())
-        return result
-    }
-    catch(e){
-        return e
-    }
-}
-
-const getSkinsByRarity = async(rarity) => {
-    try{
-        let result = await fetch(`${baseUrl}/skins.json`).then(res => res.json())
-        let filteredResult = result.filter(item => item.rarity.id == rarity)
-        return filteredResult
-    }
-    catch(e){
-        return e
-    }
-}
-
-const getSkinImageById = async(id) => {
-    try{
-        let result = await fetch(`${baseUrl}/skins.json`).then(res => res.json())
-        let filteredResult = result.filter(item => item.id == id)
-        return filteredResult
-    }
-    catch(e){
-        return e
-    }
-}
-
-const getSkinByWeaponName = async(weaponName) => {
-    try{
-        let result = await fetch(`${baseUrl}/skins.json`).then(res => res.json())
-        let filteredResult = result.filter(item => item.weapon.name == weaponName)
-        return filteredResult
-    }
-    catch(e){
-        return e
-    }
-}
-
-const getSkinByTeam = async(team) => {
-    try{
-        let result = await fetch(`${baseUrl}/skins.json`).then(res => res.json())
-        let filteredResult = result.filter(item => item.team.id === team || item.team.id === 'both')
-        let summarizedWeaponData = []
-        for(let i=0; i<filteredResult.length; i++){
-            summarizedWeaponData.push({
-                category: filteredResult[i].category.name,
-                id: filteredResult[i].id,
-                image: filteredResult[i].image,
-                skinName: filteredResult[i].name,
-                team: filteredResult[i].team.id,
-                weaponName: filteredResult[i].weapon.name,
-                rarity: filteredResult[i].rarity.id,
-            })
-        }
-        return summarizedWeaponData
-    }
-    catch(e){
-        return e
-    }
-}
-
+// Returns an object organized by {category: weaponType : [...skins]} for the skins that are valid for the team passed as an argument (being that team weapons and weapons that are available for both teams)
 const getSkinByTeamGroupedByCategoryAndWeapon = async(team) => {
     try{
+        // getting all the skins from the CS-GO API
         let result = await fetch(`${baseUrl}/skins.json`).then(res => res.json())
+        // then I filter out the skins that are not available for the team received as an argument
         let filteredResult = result.filter(item => (item.team.id === team || item.team.id === 'both'))
+        // I create an empty object that will store the new data structure with the skins information
         let summarizedDataByWeapon = {}
-        for(let i=0; i<filteredResult.length; i++){
 
+        // After getting rid of the non-compatible weapons, I go through the array of skins and reorganize the data into my new object
+        for(let i=0; i<filteredResult.length; i++){
+            // I get the category and weapon type for every weapon, since I'll be using this data to organize every skin into the custom object
+            // The rarity will be used to set the pricing of every skin later on
             let weaponName = filteredResult[i].weapon.name
             let categoryName = filteredResult[i].category.name
             let weaponRarity = filteredResult[i].rarity.id
 
+            // If I have already added a weapon for this category, I need to make sure to preserve that data while adding this new weapon
             if(summarizedDataByWeapon.hasOwnProperty(categoryName)){
+                // Same if I have already added a skin for this type of weapon
                 if(summarizedDataByWeapon[categoryName].hasOwnProperty(weaponName)){
                     summarizedDataByWeapon[categoryName][weaponName].push({
                         id: filteredResult[i].id,
@@ -89,13 +32,16 @@ const getSkinByTeamGroupedByCategoryAndWeapon = async(team) => {
                         skinName: filteredResult[i].name,
                         team: filteredResult[i].team.id,
                         rarity: filteredResult[i].rarity.id, 
+                        // I set now the price for this skin, based on the weaponPricing object from my utilities folder. For knives and gloves I calculate the price in here in order to use a new seed for the random function and not get the same value always
                         price: categoryName === 'Knives' || categoryName === 'Gloves' ? Math.round((Math.random()*8 + 2))*50 : weaponPricing[categoryName][weaponRarity],   
                     })
+                    // if the rarity was not included in this object for this category and type of weapon combination, I add it to the object to use it later
                     if(!summarizedDataByWeapon[categoryName].availableRarities.includes(weaponRarity)){
                         summarizedDataByWeapon[categoryName].availableRarities.push(weaponRarity)
                     }
                 }
                 else{
+                    // otherwise if there were already weapon types for this category, but no skins for this weapon type, I preserve the previous weapon types by assigning to my custom object the spreaded values of the previous weapon types and the new weapon type
                     summarizedDataByWeapon[categoryName] = {
                         ...summarizedDataByWeapon[categoryName],
                         [weaponName]: [{
@@ -113,6 +59,7 @@ const getSkinByTeamGroupedByCategoryAndWeapon = async(team) => {
                 }
             }
             else {
+                // in case that there were no weapons for the selected category, I just include this new category into the object, along with the weapon type and the first skin for it
                 summarizedDataByWeapon[categoryName] = {
                     [weaponName]: [{
                         id: filteredResult[i].id,
@@ -134,4 +81,4 @@ const getSkinByTeamGroupedByCategoryAndWeapon = async(team) => {
     }
 }
 
-export {getAllSkins, getSkinsByRarity, getSkinImageById, getSkinByWeaponName, getSkinByTeam, getSkinByTeamGroupedByCategoryAndWeapon}
+export {getSkinByTeamGroupedByCategoryAndWeapon}
